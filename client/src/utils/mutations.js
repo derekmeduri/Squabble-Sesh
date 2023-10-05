fa71de3aa740d2cffe7d9f7dedc6c493cf54cf28
Molly Snyder <131561844+msnyder3@users.noreply.github.com>
2023-10-04 21:22:27 -0400

    Created components and pages, started on mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,60 @@
-// import { gql } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $username: String!
+    $firstName: String!
+    $lastName: String!
+    $hottake: String!
+    $bio: Sring!
+    $email: String!
+    $password: String!
+    $fighterId: int!
+
+  ) {
+    addUser(
+      username: $username
+      firstName: $firstName
+      lastName: $lastName
+      hottake: $hottake
+      bio: $bio
+      email: $email
+      password: $password
+      fighter: $fighterId
+
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const NEW_POST = gql `
+mutation newPost ($postId: ID!, $postText: String!) {
+    newPost(postId: $postId, postText: $postText) {
+        _id
+        username
+        postText
+        createdAt
+        comments {
+            _id
+            commentText
+            createdAt
+        }
+
+    }
+}
+`;
