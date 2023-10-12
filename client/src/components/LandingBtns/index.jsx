7416a1601d1de099f0726f37ca7421355f88a1e0
SnubStumpy2007 <joshua192007@gmail.com>
2023-10-12 19:58:29 -0400

    finished up signup page styling

--- client/src/components/LandingBtns/index.jsx
+++ client/src/components/LandingBtns/index.jsx
@@ -1,14 +1,8 @@
 function LandingBtns()  {
     return (
-<<<<<<< HEAD
         <div className="button-container">
         <a href="/login" class="button">Login</a>
         <a href="/signup" class="button">Sign Up</a>
-=======
-    <div>
-        <a href="/login" className="button">Login</a>
-        <a href="/signup" className="button">Sign Up</a>
->>>>>>> e1911415b393b43560dc82d59cec5115f21c844d
     </div>
     );
 }
