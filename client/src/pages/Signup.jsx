b7b0fc1c111590b3614f073d23f9c11459855a69
SnubStumpy2007 <joshua192007@gmail.com>
2023-10-12 19:17:20 -0400

    working on signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
-import '../styles/signup.css';
+import '../styles/signupStyles.scss'
 
 function Signup(props) {
     const [formState, setFormState] = useState({ email: '', password: '' });
