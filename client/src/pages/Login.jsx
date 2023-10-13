9c013f1f0395745b8342dd1eebd524b9f27f0989
SnubStumpy2007 <joshua192007@gmail.com>
2023-10-12 21:13:34 -0400

    styled login and signup pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,58 +3,62 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
-import '../styles/login.css';
-import '../styles/login.scss'
+import '../styles/login.scss';
 
 function Login(props) {
-    const [formState, setFormState] = useState({ email: '', password: '' });
-    const [login, { error }] = useMutation(LOGIN);
-  
-    const handleFormSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        const mutationResponse = await login({
-          variables: { email: formState.email, password: formState.password },
-        });
-        const token = mutationResponse.data.login.token;
-        Auth.login(token);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-  
-    const handleChange = (event) => {
-      const { name, value } = event.target;
-      setFormState({
-        ...formState,
-        [name]: value,
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
       });
-    };
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    return (
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-        <div className="login-container">
-            <Link to="/signup">Don't have an account yet? Go to Signup</Link>
-        <h1>Login</h1>
-        <form onSubmit={handleFormSubmit}>
-            <div className="form-group">
-                <label htmlFor="username">Username:</label>
-                <input type="username" id="username" name="username" onChange={handleChange} />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" onChange={handleChange} />
-            </div>
-            {error ? (
+  const redirectToSignUp = () => {
+    // Redirect to the signup page
+    props.history.push('/signup');
+  };
+
+  return (
+    <div className="login-container">
+      <button className="btn-signup" onClick={redirectToSignUp}>
+        Don't have an account? Sign up!
+      </button>
+      <h1>Login</h1>
+      <form onSubmit={handleFormSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" name="email" onChange={handleChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" onChange={handleChange} />
+        </div>
+        {error ? (
           <div>
             <p className="error-text">The provided credentials are incorrect</p>
           </div>
         ) : null}
-            <button type="submit">Login</button>
-        </form>
+        <button className='btn-login' type="submit">Login</button>
+      </form>
     </div>
-        
-    );
+  );
 }
 
 export default Login;
