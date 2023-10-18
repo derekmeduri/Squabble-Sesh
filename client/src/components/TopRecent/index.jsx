5067cfd380331585fc97a210e1273d5e5c775c34
Molly Snyder <131561844+msnyder3@users.noreply.github.com>
2023-10-18 18:40:24 -0400

    add in delete button

--- client/src/components/TopRecent/index.jsx
+++ client/src/components/TopRecent/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_TOP_POSTS } from '../../utils/queries';
+import { QUERY_TOP_POSTS, QUERY_SINGLE_POST } from '../../utils/queries';
 // import NewComment from '../Comment';
 
 function TopPosts() {
-  const { loading, error, data } = useQuery(QUERY_TOP_POSTS);
+  const { loading, error, data } = useQuery(QUERY_TOP_POSTS, QUERY_SINGLE_POST);
   console.log(data);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -14,6 +14,12 @@ function TopPosts() {
   // Get the top 5 posts
   const top5Posts = sortedPosts.slice(0, 5);
   console.log(top5Posts);
+
+  const handleDelete = (postId) => {
+    // Create a new array without the item to delete
+    const updatedPost = post.filter(post => post.id !== postId);
+    // Update the state with the new array of items
+    setPost(updatedPost);
   
   return (
     <div className='post-container'>
@@ -22,14 +28,15 @@ function TopPosts() {
           <h2 key={post.username} id='username'>{post.username}</h2>
           <p key= {post.postText} id='post-text'>{post.postText}</p>
           <p key={post.comments.length}></p>
-          {/* <div className='comment-container'>
-            <form>
-              <NewComment/>
-            </form>
-          </div> */}
+          <div key= {post.id} id ="delete" className="col-12 col-lg-3">
+              <button className="btn btn-primary btn-block py-3" onClick={() => handleDelete(post._id)} type="delete">
+                Delete Post
+              </button>
+            </div>
         </div>
       ))}
     </div>
   );
+}
 }
 export default TopPosts;
